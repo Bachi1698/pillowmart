9795a493186943130fe08dcddaf588e8907ee24d
Bachi1698 <maryliseallegou@gmail.com>
2020-03-11 11:03:00 +0100

    Initial commit

---
+++ cart/views.py
@@ -0,0 +1,64 @@
+from django.shortcuts import render
+
+# Create your views here.
+def index(request):
+    datas = {
+
+    }
+    return render(request,'pages/index.html',datas)
+def about(request):
+    datas = {
+
+    }
+    return render(request,'pages/about.html',datas)
+def blog(request):
+    datas = {
+
+    }
+    return render(request,'pages/blog.html',datas)
+def cart(request):
+    datas = {
+
+    }
+    return render(request,'pages/cart.html',datas)
+def checkout(request):
+    datas = {
+
+    }
+    return render(request,'pages/checkout.html',datas)
+def confirmation(request):
+    datas = {
+
+    }
+    return render(request,'pages/confirmation.html',datas)
+def contact(request):
+    datas = {
+
+    }
+    return render(request,'pages/contact.html',datas)
+def elements(request):
+    datas = {
+
+    }
+    return render(request,'pages/elements.html',datas)
+def login(request):
+    datas = {
+
+    }
+    return render(request,'pages/login.html',datas)
+def product_list(request):
+    datas = {
+
+    }
+    return render(request,'pages/product_list.html',datas)
+def single_blog(request):
+    datas = {
+
+    }
+    return render(request,'pages/single-blog.html',datas)
+def single_product(request):
+    datas = {
+
+    }
+    return render(request,'pages/single-product.html',datas)
+
