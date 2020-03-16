3dbd18aa7d3baa877b18ea99bb47fb1a3c9f3b61
Bachi1698 <60926331+Bachi1698@users.noreply.github.com>
2020-03-16 18:25:35 +0000

    administration

--- blog/admin.py
+++ blog/admin.py
@@ -1,11 +1,37 @@
 from django.contrib import admin
 from . import models
+from django.utils.safestring import mark_safe
 
 # Register your models here.
-admin.site.register(models.CategorieArticle)
-admin.site.register(models.Article)
-admin.site.register(models.Commentaire)
-admin.site.register(models.Tag)
+
+class CategorieArticleAdmin(admin.ModelAdmin):
+    list_display = ('nom','date_add','date_update','status','image_view')
+    list_filter = ('status',)
+    search_fields = ('nom',)
+    date_hierarchy = "date_add"
+    list_display_links = ['nom']
+    ordering = ['nom']
+    list_per_page = 6
+    fieldsets = [
+                    ("info categorie",{"fields":["nom","image","description"]}),
+                    ("standard",{"fields":["status"]})
+                ]
+
+    def image_view(self,obj):
+        return mark_safe("<img src='{url}' width='100px',height='50px'>".format(url=obj.image.url))
+
+
+
+
+
+
+
+def _register(model,admin_class):
+    admin.site.register(model,admin_class)
+
+_register(models.CategorieArticle, CategorieArticleAdmin)
+
+
 
 
 
