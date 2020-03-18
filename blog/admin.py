7d0e66387f7784ffc6b5f80f229ef870e9401040
Bachi1698 <maryliseallegou@gmail.com>
2020-03-18 17:28:03 +0000

    admin de l'application blog

--- blog/admin.py
+++ blog/admin.py
@@ -3,6 +3,39 @@ from . import models
 from django.utils.safestring import mark_safe
 
 # Register your models here.
+class TagAdmin(admin.ModelAdmin):
+    list_display = ('nom','date_add','date_update','status',)
+    list_filter = ('status',)
+    search_fields = ('nom',)
+    date_hieararchy = "date_add"
+    ordering = ['nom']
+
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = ('titre','date_add','date_update','status','image_view')
+    list_filter = ('status',)
+    search_fields = ('titre',)
+    date_hierarchy = "date_add"
+    list_per_page = 6
+    ordering = ['titre']
+    fieldsets = [
+                    ("info article",{"fields":["titre","description","contenu","image"]})
+                    # ("standard",{"fields":["status"]})
+    ]
+    def image_view(self,obj):
+        return mark_safe("<img src'{url}' width='100px',height='50px'>".format(url=obj.iamge.url))
+
+class CommentaireAdmin(admin.ModelAdmin):
+    list_display = ('nom','prenom','date_add','date_update','image_view')
+    liste_filter = ('status',)
+    search_fields = ('nom',)
+    date_hierachy = "date_add"
+    list_display_links = ['nom']
+    ordering = ['nom']
+    list_per_page = 6
+
+    def image_view(self,obj):
+        return mark_safe("<img src='{url}' width='100px',height='50px'>".format(url=obj.image.url))
+
 
 class CategorieArticleAdmin(admin.ModelAdmin):
     list_display = ('nom','date_add','date_update','status','image_view')
@@ -26,10 +59,17 @@ class CategorieArticleAdmin(admin.ModelAdmin):
 
 
 
+
+
 def _register(model,admin_class):
     admin.site.register(model,admin_class)
 
+
 _register(models.CategorieArticle, CategorieArticleAdmin)
+_register(models.Tag,TagAdmin)
+_register(models.Article,ArticleAdmin)
+_register(models.Commentaire,CommentaireAdmin)
+
 
 
 
