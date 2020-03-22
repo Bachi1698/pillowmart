004dc7085ae362f03544f4b171959aef7ffcb27b
Bachi1698 <maryliseallegou@gmail.com>
2020-03-22 02:33:38 +0000

    image_view

--- blog/admin.py
+++ blog/admin.py
@@ -13,6 +13,9 @@ class TagAdmin(admin.ModelAdmin):
                   ("info tag",{"fields":["nom","description",]}),
                   ("standard",{"fields":["status"]})
     ]
+class ArticleInline(admin.StackedInline):
+    model = models.Article
+    extra = 0
 
 class ArticleAdmin(admin.ModelAdmin):
     list_display = ('titre','date_add','date_update','status','image_view')
@@ -56,13 +59,30 @@ class CategorieArticleAdmin(admin.ModelAdmin):
     list_display_links = ['nom']
     ordering = ['nom']
     list_per_page = 6
+    readonly_fields = ['image_view']
+    actions = ('activate','desactivate')
     fieldsets = [
                     ("info categorie",{"fields":["nom","image","description"]}),
                     ("standard",{"fields":["status"]})
                 ]
+    inlines = [
+                ArticleInline,
+    ]
+    
 
     def image_view(self,obj):
         return mark_safe("<img src='{url}' width='100px',height='50px'>".format(url=obj.image.url))
+    
+    def activate(self,request,queryset):
+        queryset.update(status=True)
+        self.message_user(request,'la selection a été effectué avec succes')
+    activate.short_description = "permet d'activer les categories selectionner"
+
+    def desactivate(self,request,queryset):
+        queryset.update(status=False)
+        self.message_user(request,'la selection a été effectué avec succes')
+    desactivate.short_description = "permet de desactiver les categories selectionner"
+
 
 
 
